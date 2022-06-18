2a030c084bffc58a44700f853efc479040a8e1d9
JKxKookie <mariaguydefontgalland@gmail.com>
2022-06-18 15:00:28 +0200

    display list of samples in Library View

--- components/Library.js
+++ components/Library.js
@@ -1,16 +1,39 @@
-import {Button, Text, View} from 'react-native';
-import {useDispatch} from "react-redux";
+import {Button, Text, TouchableOpacity, View} from 'react-native';
+import { FlatList} from "react-native";
+import React from "react";
+import {useSelector} from "react-redux";
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 
 const Library = () => {
-    //const dispatch = useDispatch();
+    const samples = useSelector((state) => state.library);
+    console.log(samples);
+
+    const sample_infos = ({ item }) => (
+        <View style={{margin:"2%", display:"flex", flexDirection:"row"}}>
+            <View style={{marginTop:13, marginRight:19}}>
+                <MusicNoteIcon style={{color:"#592304"}} sx={{ fontSize: 40 }}/>
+            </View>
+            <View>
+                <p>{item.sample}</p>
+                <p>{item.category}</p>
+            </View>
+        </View>
+    );
 
     return (
             <View>
-                <View style={{margin:"3%", fontWeight:"bold", backgroundColor:"#e3d3a6", padding:"2%", borderRadius:20}}>
+                <View style={{margin:"2%", fontWeight:"bold", backgroundColor:"#e3d3a6", padding:"2%", borderRadius:20}}>
                     <Text>
                         Vous trouverez les samples que vous avez dans votre bibliothèque.
                         Vous pouvez notamment l'agrandir en rajoutant des sons depuis la librairie Freesound.
                     </Text>
+                </View>
+
+
+                <View>
+                    <FlatList
+                        data={samples}
+                        renderItem={sample_infos}/>
                 </View>
             </View>
         );
